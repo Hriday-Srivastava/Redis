29d9ba066da921a4ea00c18f9698743402bd753f
Hriday-Kumar <heartbihar@gmail.com>
2026-04-08 10:40:33 +0530

    PUB-SUB communication using Redis ChannelTopic

---
+++ Pub-Sub-Communication-With-Redis/src/main/java/com/redis/pubsub/PubSubCommunicationWithRedisApplication.java
@@ -0,0 +1,13 @@
+package com.redis.pubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubSubCommunicationWithRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubSubCommunicationWithRedisApplication.class, args);
+	}
+
+}
---
+++ Pub-Sub-Communication-With-Redis/src/main/java/com/redis/pubsub/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.redis.pubsub.config;
+
+import com.redis.pubsub.Consumer.RedisNewsConsumer;
+import com.redis.pubsub.Consumer.RedisSportNewsConsumer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisMessageListenerContainer configConsumer(
+            RedisConnectionFactory redisConnectionFactory,
+            RedisNewsConsumer newsConsumer,
+            RedisSportNewsConsumer sportsNewsConsumer){
+        RedisMessageListenerContainer redisMessageListenerContainer
+                = new RedisMessageListenerContainer();
+
+        redisMessageListenerContainer.setConnectionFactory(redisConnectionFactory);
+        redisMessageListenerContainer.addMessageListener(newsConsumer, new ChannelTopic("News-Live"));
+        redisMessageListenerContainer.addMessageListener(sportsNewsConsumer, new ChannelTopic("News-Sports"));
+
+        return redisMessageListenerContainer;
+    }
+
+}
